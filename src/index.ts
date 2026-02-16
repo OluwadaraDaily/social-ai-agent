01c6d05208da1d9e9d2974c94acce9e3ad4b841a
Oluwadara Oloye <daraoloye99@gmail.com>
2026-02-16 13:38:30 +0100

    feat: initialize project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
